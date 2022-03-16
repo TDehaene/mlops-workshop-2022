36fd9b54fd57bd92edcf792ad1a45b2b28bdbd70
Thomas Dehaene <thomas_dehaene@hotmail.com>
2022-03-16 07:49:09 +0100

    Update setup.sh

--- workshop/local/setup.sh
+++ workshop/local/setup.sh
@@ -1,10 +1,10 @@
 # Global variables
-export PROJECT_ID="teak-backup-317206"
+export PROJECT_ID="smart-amplifier-343007"
 
 # Install requirements 
 pip install -r requirements.txt
 
 # Create GC bucket
-export BUCKET_LOCAL_RUN=${PROJECT_ID}_local-run
+export BUCKET_LOCATION=europe-west1-b
+export BUCKET_LOCAL_RUN=${PROJECT_ID}-modeltraining
 gsutil mb -p $PROJECT_ID -l $BUCKET_LOCATION gs://$BUCKET_LOCAL_RUN
-
